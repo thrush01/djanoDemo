9c6a7163daaad5896a41218a8e6368d67d6cb8b9
LJKekana <keygley@gmail.com>
2024-05-23 10:42:18 +0200

    pictures fixed,sign up form creates

--- demo/POLL/views.py
+++ demo/POLL/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from items.models import Categories,Item
 # Create your views here.
 def index(request):
-    return render(request,'demo/index.html')
+    items=Item.objects.filter(is_sold=False)
+    categories=Categories.objects.all()
+
+    return render(request,'demo/index.html',{
+        'items':items,
+        'categories':categories,
+    })
 
 def content(request):
     return render(request,'demo/index.html')
--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,10 +1,12 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from POLL.views import index,content
 
 urlpatterns = [
     path('',index,name='index'),
+    path('items/',include('item.urls')),
     path('content/',content,name='content'),
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
