7aab5a4db5a439588288e210dd6e8f1769498c77
LJKekana <keygley@gmail.com>
2024-05-23 22:58:32 +0200

    search field and clear buttons added...conversation app created

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'POLL',
     'items',
-    'dashboard'
+    'dashboard',
+    'conversation',
 ]
 
 MIDDLEWARE = [
--- demo/items/views.py
+++ demo/items/views.py
@@ -1,14 +1,26 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render,get_object_or_404,redirect
 
 from .forms import NewItemForm,EditItemForm
-from .models import Item
+from .models import Categories, Item
 
 def items(request):
+    query=request.GET.get('query','')
+    category_id=request.GET.get('category',0)
+    categories=Categories.objects.all()
     items=Item.objects.filter(is_sold=False)
 
+    if category_id:
+        items=items.filter(category_id=category_id)
+
+    if query:
+        items=items.filter(Q(name__contains=query))
     return render(request,'item//items.html',{
         'items':items,
+        'query':query,
+        'categories':categories,
+        'category_id':int(category_id)
     })
 def detail(request,pk):
     item=get_object_or_404(Item,pk=pk)
