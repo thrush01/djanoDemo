2bd75160c3e579f0d30e24ad33c9611f41a6a417
LJKekana <keygley@gmail.com>
2024-05-24 02:06:41 +0200

    inbox not complete

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path, include
 
 urlpatterns = [
-    path('',include('POLL.urls')),
-    path('items/',include('items.urls')),
-    path('dashboard/',include('dashboard.urls')),
+    path('', include('POLL.urls')),
+    path('items/', include('items.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
